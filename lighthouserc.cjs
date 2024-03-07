429278574799e7b18ba21735fd11a2631826f806
Annas Baig <annas.baig@iomechs.com>
2024-03-07 14:51:54 +0500

    change url to random url

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -3,7 +3,7 @@ module.exports = {
       collect: {
         startServerReadyPattern: 'Local', // This phrase prints out after preview server starts
         startServerCommand: 'npm run preview',
-        url: 'http://localhost:4200',
+        url: 'http://localhost:4209',
         numberOfRuns: 1,
       },
       upload: {
