4252fa83b96cd46d7658ad1d379898bfd213b4df
Annas Baig <annas.baig@iomechs.com>
2024-03-11 14:10:32 +0500

    secrets changed

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,16 +1,15 @@
-// const { environment } = require('./src/environments/environment');
-// module.exports = {
-//     ci: {
-//       collect: {
-//         startServerReadyPattern: 'Local', // This phrase prints out after preview server starts
-//         startServerCommand: 'npm run preview',
-//         url: process.env.AUDIT_URL,
-//         numberOfRuns: 1,
-//       },
-//       upload: {
-//         target: 'lhci',
-//         serverBaseUrl: environment.lhciServerURL,
-//         token: environment.lhciBuildToken, // build token from lhci wizard
-//       },
-//     },
-//   }; 
+module.exports = {
+    ci: {
+      collect: {
+        startServerReadyPattern: 'Local', // This phrase prints out after preview server starts
+        startServerCommand: 'npm run preview',
+        url: process.env.AUDIT_URL,
+        numberOfRuns: 1,
+      },
+      upload: {
+        target: 'lhci',
+        serverBaseUrl: process.env.LHCI_SERVER_URL,
+        token: process.evn.LHCI_BUILD_TOKEN, // build token from lhci wizard
+      },
+    },
+  }; 
