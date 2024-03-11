8fe8a4363a142703dc342659ec2a92ef886af9d7
Annas Baig <annas.baig@iomechs.com>
2024-03-11 13:01:14 +0500

    added dotenv package

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -3,13 +3,13 @@ module.exports = {
       collect: {
         startServerReadyPattern: 'Local', // This phrase prints out after preview server starts
         startServerCommand: 'npm run preview',
-        url: 'http://localhost:4200',
+        url: process.env.AUDIT_URL,
         numberOfRuns: 1,
       },
       upload: {
         target: 'lhci',
-        serverBaseUrl: 'https://lighthouse-server-production-8084.up.railway.app',
-        token: '0ac98669-2c1f-4b1d-99c2-4acd8f163e45', // build token from lhci wizard
+        serverBaseUrl: process.env.LHCI_SERVER_URL,
+        token: process.env.LHCI_BUILD_TOKEN, // build token from lhci wizard
       },
     },
   }; 
