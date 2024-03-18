fea04eff54dd7f44b36b72c1e15ca830c18304fc
Annas Baig <annas.baig@iomechs.com>
2024-03-18 15:20:00 +0500

    outputDir path changed

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -10,15 +10,12 @@ module.exports = {
           preset: 'desktop',
         }
       },
-      settings: {
-      output: ["json"] // Include "json" in the output array
-  }
-      // upload: {
-      //   target: 'filesystem',
-      //   outputDir: 'lhreports'
-      //   // serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
-      //   // token: process.env.LHCI_SERVER_TOKEN, // build token from lhci wizard
-      // },
+      upload: {
+        target: 'filesystem',
+        outputDir: './lhci'
+        // serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
+        // token: process.env.LHCI_SERVER_TOKEN, // build token from lhci wizard
+      },
       // assert: {
       //   assertions: {
       //     "categories:seo" : ["warn", {"minScore": 0.85}],
