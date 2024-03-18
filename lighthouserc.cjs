4eb011ee3514ca24e7fe73af3cf38b490ffdd335
Annas Baig <annas.baig@iomechs.com>
2024-03-18 13:15:36 +0500

    LH report on build

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,19 +1,20 @@
 module.exports = {
     ci: {
-      // collect: {
-      //   startServerReadyPattern: 'Compiled successfully', // This phrase prints out after preview server starts
-      //   startServerCommand: 'npm run preview',
-      //   url: process.env.AUDIT_URL,
-      //   numberOfRuns: 5,
-      //   settings: {
-      //     preset: 'desktop',
-      //   }
-      // },
-      // upload: {
-      //   target: 'lhci',
-      //   serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
-      //   token: process.env.LHCI_SERVER_TOKEN, // build token from lhci wizard
-      // },
+      collect: {
+        // startServerReadyPattern: 'Compiled successfully', // This phrase prints out after preview server starts
+        // startServerCommand: 'npm run preview',
+        // url: process.env.AUDIT_URL,
+        staticDistDir: 'dist/todo-app',
+        numberOfRuns: 3,
+        settings: {
+          preset: 'desktop',
+        }
+      },
+      upload: {
+        target: 'lhci',
+        serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
+        token: process.env.LHCI_SERVER_TOKEN, // build token from lhci wizard
+      },
       // assert: {
       //   assertions: {
       //     "categories:seo" : ["warn", {"minScore": 0.85}],
