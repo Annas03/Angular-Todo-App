52bdbeaf850d6fe4e516ff4be9522d41465f72ad
Annas Baig <annas.baig@iomechs.com>
2024-03-26 11:56:08 +0500

    recipe 3 ran on localhost

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -17,27 +17,6 @@
 // };
 
 // recipe 2: Save LH report on LH server
-module.exports = {
-  ci: {
-    collect: {
-      startServerReadyPattern: "Compiled successfully",
-      startServerCommand: "npm run preview",
-      url: process.env.AUDIT_URL,
-      numberOfRuns: 5,
-      settings: {
-        preset: "desktop",
-      },
-    },
-    upload: {
-      target: "lhci",
-      serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
-      token: process.env.LHCI_SERVER_TOKEN,
-    },
-  },
-};
-
-
-// recipe 3: Generated LH report asserted
 // module.exports = {
 //   ci: {
 //     collect: {
@@ -54,10 +33,31 @@ module.exports = {
 //       serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
 //       token: process.env.LHCI_SERVER_TOKEN,
 //     },
-//     assert: {
-//       assertions: {
-//         "categories:seo": ["warn", { minScore: 0.85 }],
-//       },
-//     },
 //   },
 // };
+
+
+// recipe 3: Generated LH report asserted
+module.exports = {
+  ci: {
+    collect: {
+      startServerReadyPattern: "Compiled successfully",
+      startServerCommand: "npm run preview",
+      url: process.env.AUDIT_URL,
+      numberOfRuns: 5,
+      settings: {
+        preset: "desktop",
+      },
+    },
+    upload: {
+      target: "lhci",
+      serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
+      token: process.env.LHCI_SERVER_TOKEN,
+    },
+    assert: {
+      assertions: {
+        "categories:seo": ["warn", { minScore: 0.85 }],
+      },
+    },
+  },
+};
