b4d66f78ecb0740465962f9dff318137313bb70e
Annas Baig <annas.baig@iomechs.com>
2024-03-26 11:46:44 +0500

    recipe 2 ran

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,26 +1,8 @@
 // recipe 1: Generating a LH report and saving it to temporary storage
-module.exports = {
-  ci: {
-    collect: {
-      startServerReadyPattern: "Compiled successfully", // This phrase prints out after preview server starts
-      startServerCommand: "npm run preview",
-      url: process.env.AUDIT_URL,
-      numberOfRuns: 5,
-      settings: {
-        preset: "desktop",
-      },
-    },
-    upload: {
-      target: "temporary-public-storage",
-    },
-  },
-};
-
-// recipe 2: Save LH report on LH server
 // module.exports = {
 //   ci: {
 //     collect: {
-//       startServerReadyPattern: "Compiled successfully",
+//       startServerReadyPattern: "Compiled successfully", // This phrase prints out after preview server starts
 //       startServerCommand: "npm run preview",
 //       url: process.env.AUDIT_URL,
 //       numberOfRuns: 5,
@@ -29,12 +11,30 @@ module.exports = {
 //       },
 //     },
 //     upload: {
-//       target: "lhci",
-//       serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
-//       token: process.env.LHCI_SERVER_TOKEN,
+//       target: "temporary-public-storage",
 //     },
 //   },
 // };
+
+// recipe 2: Save LH report on LH server
+module.exports = {
+  ci: {
+    collect: {
+      startServerReadyPattern: "Compiled successfully",
+      startServerCommand: "npm run preview",
+      url: process.env.AUDIT_URL,
+      numberOfRuns: 5,
+      settings: {
+        preset: "desktop",
+      },
+    },
+    upload: {
+      target: "lhci",
+      serverBaseUrl: process.env.LHCI_SERVER_BASE_URL,
+      token: process.env.LHCI_SERVER_TOKEN,
+    },
+  },
+};
 
 // recipe 3: Generated LH report asserted
 // module.exports = {
